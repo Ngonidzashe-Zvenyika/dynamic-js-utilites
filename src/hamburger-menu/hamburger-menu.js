5b1f0aed6695b38653d86eb9af32b44a72a858db
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-09-25 12:52:53 +0200

    Add a module for hamburger menus

---
+++ src/hamburger-menu/hamburger-menu.js
@@ -0,0 +1,27 @@
+function setHamburgerMenus() {
+  const hamburgerMenus = document.querySelectorAll('.hamburger-menu');
+
+  hamburgerMenus.forEach((menu) => {
+    const openMenu = menu.querySelector('.open');
+    const closeMenu = menu.querySelector('.close');
+    const options = menu.querySelector('.options');
+
+    openMenu.addEventListener('click', () => {
+      openMenu.classList.toggle('active');
+      closeMenu.classList.toggle('active');
+      options.classList.toggle('active');
+    });
+    closeMenu.addEventListener('click', () => {
+      openMenu.classList.toggle('active');
+      closeMenu.classList.toggle('active');
+      options.classList.toggle('active');
+    });
+    options.addEventListener('click', () => {
+      openMenu.classList.toggle('active');
+      closeMenu.classList.toggle('active');
+      options.classList.toggle('active');
+    });
+  });
+}
+
+exports.setHamburgerMenus = setHamburgerMenus;
