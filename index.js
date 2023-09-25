b5de5642589c31dcdfaff5652d7cb8ba2c7f0187
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-09-25 12:54:57 +0200

    Import modules in index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const setDropdownMenus = require('./src/drop-down-menu/drop-down-menu.js');
+const setHamburgerMenus = require('./src//hamburger-menu/hamburger-menu.js');
+const setCarouselsOpacity = require('./src//image-carousel-opacity/image-carousel-opacity.js');
+const setCarouselsTransform = require('./src/image-carousel-transform/image-carousel-transform.js');
+
+module.exports = {
+  setDropdownMenus,
+  setHamburgerMenus,
+  setCarouselsOpacity,
+  setCarouselsTransform,
+};
